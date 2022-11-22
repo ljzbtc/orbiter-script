bc194173391a5da62c67715ce5ab08bcaa128117
Psycholab.ljz <psyljz@gmail.com>
2022-11-22 21:45:18 +0800

    move code inro src dir

--- AceOpBSC.sh
+++ AceOpBSC.sh
@@ -3,13 +3,13 @@ echo $(date)
 echo "==========脚本开始执行共计划执行$1次======"
 for i in $(seq 1 $1)  
 do   
-node opToBsc.js
+node ./src/opToBsc.js
 echo 休息$2s
 sleep $2 
 done 
 echo "========OP向BSC的已完成======="
 
 
-node BsctoOp.js
+node ./src/BsctoOp.js
 echo "=========脚本结束========="
 
